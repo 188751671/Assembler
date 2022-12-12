642bb73303900c310f01d85913b1f5de86bc46ec
Ace <doubleace188@gmail.com>
2022-12-12 16:09:39 +0000

    Update BinFileWriter.java

--- Assembler/src/BinFileWriter.java
+++ Assembler/src/BinFileWriter.java
@@ -1,29 +1,33 @@
 import java.io.*;
-
 class BinFileWriter {
     static FileOutputStream fileWriter;
-
+    public static String buffer = "";
     static {
         try {
-            fileWriter = new FileOutputStream(new File(Assembler.newFilename));
+            fileWriter = new FileOutputStream(Assembler.newFilename);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
-
     static int nextCodeAddr = 0;
 
     public BinFileWriter() throws IOException {
         fileWriter = new FileOutputStream(new File(Assembler.newFilename));
     }
+    public static int getNextCodeAddr() {
+        return nextCodeAddr;
+    }
 
-    static public boolean writeBits(String binaryString) throws IOException {
-        String High8 = binaryString.substring(0,8);
-        String Low8 = binaryString.substring(8,16);
-        fileWriter.write(Integer.parseInt(High8,2));
-        fileWriter.write(Integer.parseInt(Low8,2));
+    static public boolean writeBits(String binaryString) {
+        buffer = buffer + binaryString +"\n";
         nextCodeAddr++;
         return true;
     }
 
+    static public void writeFile() throws IOException {
+        fileWriter.write(buffer.getBytes());
+    }
+    static public void writeFile(String file) throws IOException{
+        fileWriter.write(file.getBytes());
+    }
 }
