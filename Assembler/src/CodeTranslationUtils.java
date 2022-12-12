8dc3c8c74e243bfc6455ed341817e6ddc2b6e248
Ace <doubleace188@gmail.com>
2022-12-12 16:10:10 +0000

    Update CodeTranslationUtils.java

--- Assembler/src/CodeTranslationUtils.java
+++ Assembler/src/CodeTranslationUtils.java
@@ -12,17 +12,26 @@ class CodeTranslationUtils{
         return GapFillerWith0(binaryString,3);
     }
     public boolean isConstant(String number){    //  0-32767
-        if(number.matches("^#[1-9][0-9]{0,4}+"))
+        if(number.startsWith("#"))
             if (Integer.parseInt(number.substring(1,number.length()))<=32767)
                 return true;
-
+        if (number.matches("[0-9]{1,5}"))
+            return true;
         return false;
     }
     //      Constant is smaller than 32767 which takes up 15bits only.
     //      So if greater than 6bits, it uses the next 16bits, the 6bits above sets 0
     public String getConstantInBinary(String constant){
-        String cons = constant.substring(1,constant.length()); // trim off '#'
-        String binaryString = Integer.toBinaryString(Integer.parseInt(cons));
+
+        String binaryString;
+        if(constant.startsWith("#")){
+            String cons = constant.substring(1,constant.length()); // trim off '#'
+            binaryString = Integer.toBinaryString(Integer.parseInt(cons));
+        }else {
+            binaryString = Integer.toBinaryString(Integer.parseInt(constant));
+        }
+
+
         if (binaryString.length()<=6){
             return GapFillerWith0(binaryString,6);
         }else {
